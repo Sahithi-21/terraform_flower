d3acc349403ce3db175e47a95954aa44ba28b14f
user46cl4 <user46cl4@ldap.com>
2023-09-27 16:02:57 +0530

    adding terraform files for virtual machine

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "resourcename" {
+  default = "example-resources"
+}
+variable "vnet" {
+  default = "example-network"
+}
+variable "subnet" {
+  default = "internal"
+}
+variable "nic" {
+  default = "example-nic"
+}
+variable "vm" {
+  default = "example-machine"
+}
